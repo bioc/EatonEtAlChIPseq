f8b404316975f8f3c2bdb008f8e944905dd2a288
Dan Tenenbaum <dtenenba@fhcrc.org>
2012-03-22 18:15:00 +0000

    rtracklayer import() wants file:/// urls on windows
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/EatonEtAlChIPseq@2025 db2202da-8704-0410-b924-b125b3cd84ef

--- data/orcPeaksRep1.R
+++ data/orcPeaksRep1.R
@@ -1,8 +1,8 @@
 library(rtracklayer)
 orcPeaksRep1 <-
-  suppressWarnings(as(import(system.file("extdata", 
+  suppressWarnings(as(import(paste("file:///", system.file("extdata", 
                                          "GSM424494_wt_G2_orc_chip_rep1_14.bed",
-                                         package = "EatonEtAlChIPseq"),
+                                         package = "EatonEtAlChIPseq"), sep=""),
                              format = "bed"), "GRanges"))
 elementMetadata(orcPeaksRep1)[["name"]] <- NULL
 seqlevels(orcPeaksRep1) <- "chrXIV"
--- data/orcPeaksRep2.R
+++ data/orcPeaksRep2.R
@@ -1,8 +1,8 @@
 library(rtracklayer)
 orcPeaksRep2 <-
-  suppressWarnings(as(import(system.file("extdata", 
+  suppressWarnings(as(import(paste("file:///" ,system.file("extdata", 
                                          "GSM424494_wt_G2_orc_chip_rep2_14.bed",
-                                         package = "EatonEtAlChIPseq"),
+                                         package = "EatonEtAlChIPseq"), sep=""),
                              format = "bed"), "GRanges"))
 elementMetadata(orcPeaksRep2)[["name"]] <- NULL
 seqlevels(orcPeaksRep2) <- "chrXIV"
